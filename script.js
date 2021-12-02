2a624d938c183d8894d55748c1b95cf8fce951c3
Altun0 <91114236+Altun0@users.noreply.github.com>
2021-12-03 00:52:59 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+      this.previousOperandTextElement = previousOperandTextElement
+      this.currentOperandTextElement = currentOperandTextElement
+      this.clear()
+  }
+  
+  clear() {
+}
+
+delete() {
+}
+
+appendNumber(number) {
+}
+
+chooseOperation(operation) {
+}
+
+compute() {
+}
+
+updateDisplay() {
+}
+
+clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
+  }
+ 
+
